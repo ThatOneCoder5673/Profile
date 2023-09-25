4b7fc03f2a726eb73e5032f321de5b06998f6ac0
ThatOneCoder5673 <144918406+ThatOneCoder5673@users.noreply.github.com>
2023-09-25 16:34:00 +0000

    yes

--- Functions.js
+++ Functions.js
@@ -29,13 +29,15 @@ var x = document.getElementById("multitask2");
 }
 function ShowContact() {
     var x = document.getElementById("mycontact");
-    if (x.style.display === "none") {x.style.display = "flex";}
-    else {x.style.display = "none"};
+    x.style.display = "flex";
 }
 function ShowLogin() {
     var x = document.getElementById("LoginModalId");
     if (x.style.display === "none") {x.style.display = "block";}
     else {x.style.display = "none"};
 }
-
-
+function CloseLogin() {
+    var x = document.getElementById("LoginModalId");
+    if (x.style.display === "none") {x.style.display = "block";}
+    else {x.style.display = "none"};
+}
